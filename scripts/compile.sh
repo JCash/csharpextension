68fde7476789163450f39c4fa16db43f3ac94428
JCash <mwesterdahl76@gmail.com>
2024-08-15 11:50:02 +0200

    Updated build scripts

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -20,7 +20,7 @@ DOTNET_MAJOR_VERSION=$(dotnet --info | dotnet --version | awk -F'.' '{print $1}'
 DOTNET_VERSION=$(dotnet --version)
 DOTNET_SDK_VERSION=$(dotnet --info | python -c "import sys; lns = sys.stdin.readlines(); i = lns.index('Host:\n'); print(lns[i+1].strip().split()[1])")
 
-AOTBASE=${NUGET_DIR}/runtime.${OS}-${ARCH}.microsoft.dotnet.ilcompiler/${DOTNET_SDK_VERSION}
+AOTBASE=${NUGET_DIR}/microsoft.netcore.app.runtime.nativeaot.osx-${ARCH}/${DOTNET_SDK_VERSION}/runtimes/osx-${ARCH}/native
 echo AOTBASE=${AOTBASE}
 
 PUBLISH_DIR=./NativeLibrary/bin/Release/net${DOTNET_MAJOR_VERSION}.0/${OS}-${ARCH}/native
@@ -43,18 +43,18 @@ dotnet publish -c Release -r ${OS}-${ARCH} ${PROJ}
 # patch the library for macos (issue https://github.com/dotnet/runtime/issues/96663)
 #python ./scripts/patch_macho.py ${PUBLISH_DIR}/libNativeLibrary.a
 
-(cd machorepack && cargo build)
+#(cd machorepack && cargo build)
 
 #./machorepack/target/debug/machorepack ${PUBLISH_DIR}/libNativeLibrary.a ${BUILD_DIR}/libNativeLibraryPatched.a
 cp -v ${PUBLISH_DIR}/libNativeLibrary.a ${BUILD_DIR}/
 
 
-OPT=-O2
+#OPT=-O2
 #OPT="-g -O0"
-#OPT="-g -O2 -flto"
+OPT="-g -O2 -flto"
 DOSTRIP=true
 
-MIN_OSX_VERSION=11.0
+MIN_OSX_VERSION=12.0
 
 clang -c ${OPT} -I./src/external -o ${BUILD_DIR}/clib.o ./src/clib.c
 ar rcs ${BUILD_DIR}/libclib.a ${BUILD_DIR}/clib.o
@@ -68,13 +68,14 @@ clang++ ${OPT} -o ${BUILD_DIR}/test \
     -lclib \
     -llua51 \
     -lNativeLibrary \
-    ${AOTBASE}/sdk/libbootstrapperdll.o \
-    ${AOTBASE}/sdk/libRuntime.WorkstationGC.a \
-    ${AOTBASE}/sdk/libeventpipe-enabled.a \
-    ${AOTBASE}/sdk/libstandalonegc-enabled.a \
-    ${AOTBASE}/framework/libSystem.Native.a \
-    ${AOTBASE}/framework/libSystem.IO.Compression.Native.a \
-    ${AOTBASE}/framework/libSystem.Globalization.Native.a \
+    ${AOTBASE}/libbootstrapperdll.o \
+    ${AOTBASE}/libRuntime.WorkstationGC.a \
+    ${AOTBASE}/libRuntime.VxsortEnabled.a \
+    ${AOTBASE}/libeventpipe-enabled.a \
+    ${AOTBASE}/libstandalonegc-enabled.a \
+    ${AOTBASE}/libSystem.Native.a \
+    ${AOTBASE}/libSystem.IO.Compression.Native.a \
+    ${AOTBASE}/libSystem.Globalization.Native.a \
     -Wl,-rpath,@executable_path/ \
     -framework Foundation \
     src/main.cpp
--- scripts/compile_external.sh
+++ scripts/compile_external.sh
@@ -16,12 +16,17 @@ LUA_DIR=./src/external/lua
 
 echo "Compiling Lua 5.1"
 
+ARCH=arm64
+if [ "i386" == "$(arch)" ]; then
+    ARCH=x86_64
+fi
+
 TARGET=
 if [ "${PLATFORM}" == "ios" ]; then
     SDK=$(xcrun -f --sdk iphoneos --show-sdk-path)
-    TARGET="-arch arm64 -miphoneos-version-min=11.0 -isystem ${SDK}/usr/include/c++/v1 -isysroot ${SDK} -DLUA_USE_IOS"
+    TARGET="-arch ${ARCH} -miphoneos-version-min=11.0 -isystem ${SDK}/usr/include/c++/v1 -isysroot ${SDK} -DLUA_USE_IOS"
 elif [ "${PLATFORM}" == "macos" ]; then
-    TARGET="-arch arm64 -target arm64-apple-darwin19"
+    TARGET="-arch ${ARCH} -target ${ARCH}-apple-darwin19"
 fi
 
 mkdir -p ${BUILD_DIR}
